58f6eb385e757c02defecf63d423965c5e3a9bac
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-08-18 08:28:45 +0300

    Skills

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Icon} from "../../../components/icon/Icon";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-<SectionTitle>My Skills</SectionTitle>
+            <SectionTitle>My Skills</SectionTitle>
             <FlexWrapper>
-                <Skill>
-                    <Icon iconId={'codeSvg'}/>
-                    <SkillTitle>html5</SkillTitle>
-                    <SkillText>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim</SkillText>
-
-                </Skill>
+                <Skill/>
+                <Skill/>
+                <Skill/>
             </FlexWrapper>
         </StyledSkills>
     );
